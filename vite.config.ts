667206ff5ccb31387ea2694fd2191e379f1e9e1c
asoft <myagkiyalexandr@hotmail.com>
2025-03-10 19:30:21 +0200

    Fix mkcert dependency inclusion

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import mkcert from 'vite-plugin-mkcert';
 import { config } from 'dotenv';
 
 config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
